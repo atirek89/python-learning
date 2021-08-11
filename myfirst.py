316cd4cd51ffe916fd030ab2326179e6afdf2071
Ati <er.atirek@gmail.com>
2021-08-12 02:17:42 +0530

    special arguments in print

--- myfirst.py
+++ myfirst.py
@@ -3,3 +3,8 @@ print("Hello, This is my first python program!!")
 print("Hello, \nThis is a new line print!!")
 
 print("Hello,", "This", "is", "a", "multi argument", "print!!")
+
+# special arguments in print
+
+print("Hello", end=", ")
+print("This is a new line print!!")
