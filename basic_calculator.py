d36faca4553775580072a16dc1191d30c4cda28c
Ati <er.atirek@gmail.com>
2021-08-12 22:24:26 +0530

    Basic Calculator

---
+++ basic_calculator.py
@@ -0,0 +1,10 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+result = float(num1) + float(num2)
+
+print("Answer is:", result)
+
+# result = int(num1) + int(num2)
+
+# print("Answer is:", result)
