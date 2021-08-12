34c7deccbb2aa8a03e879cd3b108921d24263fee
Ati <er.atirek@gmail.com>
2021-08-12 18:41:56 +0530

    Arithmetic in python

---
+++ numbers.py
@@ -0,0 +1,31 @@
+# Addition
+print("Addition:", 2+3)
+
+# Substraction
+print("Substraction:", 5-2)
+
+# Multiplication
+print("Multiplication:", 3*2)
+
+# Division
+print("Division:", 7/2)
+
+# Floor Division
+print("Floor Division:", 7/2)  # returns only integer result without rounding the decimal
+
+# Modulo
+print("Modulo:", 7 % 2)
+
+# Powers
+print(2**3)
+
+# Can also do roots this way
+print(4**0.5)
+
+# Order of Operations followed in Python
+print(2 + 10 * 10 + 3)
+
+# Can use parentheses to specify orders
+print((2+10) * (10+3))
+
+print("floating point accuracy:", 0.1+0.2-0.3)
